2e4e6c16b6881b8ea4c00cfbac4a320bc6998b9a
Dani Conde - Portátil <daniel.burbano@est.fib.upc.edu>
2015-11-24 16:26:29 +0100

    AICurrent.cc: Adding info searching functions

--- AICurrent.cc
+++ AICurrent.cc
@@ -1,9 +1,8 @@
-
 #include "Player.hh"
 #include <vector>
 #include <stack>
 #include <queue>
-#include <pair>
+#include <complex>
 using namespace std;
 
 
@@ -27,28 +26,42 @@ struct PLAYER_NAME : public Player {
     static Player* factory () {
         return new PLAYER_NAME;
     }
-
-
+    Dir movez = UP;
+    Player_Id myId;
     int n_rocket_plus;
     int n_coins;
     int n_players;
+    set<Player_Id>Ids;
+    set<Starship_Id> enemies;
+    set<Starship_Id> allies;
     complex<int> map_dimensions;
     complex<int> win_dimensions;
     map<int, bool> map_registry;
     /**
      * Attributes for your player can be defined here.
      */
+    void get_allies(){
+        for (Starship_Id sid = begin(myId); sid < end(myId); ++sid){
+            allies.insert(sid);
+        }
+    }
 
     void read_map(){
         for (int i = 0; i < map_dimensions.first){
             for (int j = 0; j < map_dimensions.second){
-                complex<int, int> position = {i, j};
+                Pos pos = {i, j};
+                Cell c = c(pos);
+                if (c.type = STARSHIP){
+                    if (starship(c.sid))
+                }
+
             }
         }
     }
 
     void initialize_game(){
         n_players = number_players;
+        get_allies();
         map_dimensions = {number_rows(), number_universe_columns()};
         win_dimensions = {first(map_dimensions), number_window_columns()};
         read_map();
