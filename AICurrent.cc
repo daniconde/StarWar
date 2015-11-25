ad5338e1707b2925c333ce4b23ad6e8c8bfa6e3b
Dani Conde - Portátil <daniel.burbano@est.fib.upc.edu>
2015-11-25 01:00:22 +0100

    Added qtcreator project files Added code to AICurrent.cc

--- AICurrent.cc
+++ AICurrent.cc
@@ -3,6 +3,8 @@
 #include <stack>
 #include <queue>
 #include <complex>
+#include <list>
+#include <map>
 using namespace std;
 
 
@@ -17,8 +19,6 @@ using namespace std;
 
 
 struct PLAYER_NAME : public Player {
-
-
     /**
      * Factory: returns a new instance of this class.
      * Do not modify this function.
@@ -26,57 +26,149 @@ struct PLAYER_NAME : public Player {
     static Player* factory () {
         return new PLAYER_NAME;
     }
-    Dir movez = UP;
-    Player_Id myId;
-    int n_rocket_plus;
-    int n_coins;
-    int n_players;
-    set<Player_Id>Ids;
-    set<Starship_Id> enemies;
-    set<Starship_Id> allies;
-    complex<int> map_dimensions;
-    complex<int> win_dimensions;
-    map<int, bool> map_registry;
-    /**
-     * Attributes for your player can be defined here.
-     */
-    void get_allies(){
-        for (Starship_Id sid = begin(myId); sid < end(myId); ++sid){
-            allies.insert(sid);
+
+    /*---------------------EDIT FROM HERE--------------------------------------*/
+
+bool initial_control = true;
+//A step is an action, which can be a move in dir if (is_move) or a shoot
+struct Step {
+    bool is_move;
+    Dir dir;
+};
+
+//A mision consists of a list of steps to reach a position objective
+struct Mission {
+    list<Step> steps;
+    Pos objective;
+};
+
+struct Assignament {
+    Mission mission;
+    Starship_Id sid;
+};
+
+//A tactic cell contains the predicted dangers for that cell and auxiliary info used
+//by starships to calculate paths
+struct TacticalCell{
+    //First element indicates the round in which the cell will be inaccesible
+    //This danger is only valid if round() equals the second value.
+    map<int, int> dangers;
+
+    //Auxiliary values for starship pathing
+    int distance;
+    Starship_Id updater;
+    int round;
+};
+
+
+
+
+//Stores me()
+Player_Id myId;
+
+//Stores the Starship_Ids of enemoy starships.
+set<Starship_Id> enemies;
+
+//Stores round() at the start of each round
+int myround;
+
+//Stores the Starships_Ids of allies, along with their current missions.
+vector<Assignament> assignament;
+
+//Stores information about the map, distribted in tactical cells.
+vector< vector<TacticalCell> > tactical_map;
+
+/**
+ * Attributes for your player can be defined here.
+ */
+
+//Get allies and game dimensions
+void initialize_game();
+
+//Draw danger zones into tactical map;
+void draw_dangers();
+
+//Writes into assignament a mission for the specified starship
+Mission get_mission(Starship_Id sid);
+
+//Does the action specified by step. If it coudln't be done, returns false
+bool do_step(Assignament &assignament);
+
+virtual void play () {
+    if (initial_control) initialize_game();
+    draw_dangers();
+    for (int i = 0; i < assignament.size(); ++i){
+        Starship ship = starship(assignament[i]);
+        if (ship.alive and not do_step(assignament[i])){
+            assignament[i].mission = get_mission(assignament[i].sid);
+            do_step(assignament[i]);
         }
     }
+}
 
-    void read_map(){
-        for (int i = 0; i < map_dimensions.first){
-            for (int j = 0; j < map_dimensions.second){
-                Pos pos = {i, j};
-                Cell c = c(pos);
-                if (c.type = STARSHIP){
-                    if (starship(c.sid))
-                }
+Mission get_mission(Starship_Id sid){
+    Pos pos =
+}
 
+void initialize_game(){
+    //Get ally Starship_Ids into mission_of
+    if (round() == 0){
+        myId = me();
+        int n_spp = number_starships_per_player();
+        mission_of.resize(n_spp);
+        Starship_Id bsid = begin(myId);
+        Starship_Id esid = end(myId);
+        for (Starship_Id sid = bsid; sid < esid; ++sid){
+            assignament[sid - bsid].sid = sid;
+        }
+    }
+    //Get map and window dimensions
+    //map_dimensions = {number_rows(), number_universe_columns()};
+    //win_dimensions = {first(map_dimensions), number_window_columns()};
+
+    //Get enemy Starship_Ids into enemy;
+    Pos win_dimensions = {number_rows(), number_window_columns()};
+    int n_p = number_players() - enemies.size();
+    int i = 0;
+    while (i < first(win_dimensions) and n_p > 0){
+        int j = 0;
+        while (j < second(win_dimensions) and n_p > 0){
+            Cell c = cell(Pos{i, j});
+            if (c.type == STARSHIP and player_of(c.sid) != myId
+            and enemies.find(player_of(c.sid)) != enemies.end()){
+                --n_p;
+                enemies.insert(player_of(c.sid));
             }
+            ++j;
         }
+        ++i;
     }
+    if (n_p == 0) initial_control = false;
+}
 
-    void initialize_game(){
-        n_players = number_players;
-        get_allies();
-        map_dimensions = {number_rows(), number_universe_columns()};
-        win_dimensions = {first(map_dimensions), number_window_columns()};
-        read_map();
+
+void draw_dangers(){
+    map<Missile_Id, Missile> mymissiles = missiles();
+    map<Missile_Id, Missile>::const_iterator mis_it = mymissiles.begin();
+    map<Missile_Id, Missile>::const_iterator mis_fi = mymissiles.end();
+    while (mis_it != mis_fi){
+        /*!!!!!!!!!!!!!!!!*/
+        trace_missile_path(*mis_it);
+        ++mis_it;
     }
-    /**
-     * Play method.
-     *
-     * This method will be invoked once per each round.
-     * You have to read the board here to place your actions
-     * for this round.
-     *
-     */
-    virtual void play () {
-        if (round() == 0) initialize_game();
+    set<Starship_Id>::const_iterator ene_it = enemies.begin();
+    set<Starship_Id>::const_iterator ene_fi = enemies.end();
+    while (ene_it != ene_fi){
+        /*!!!!!!!!!!!!!!!*/
+        trace_enemy_danger(*ene_it);
+        ++ene_it;
     }
+}
+
+
+
+
+
 
 
 };
